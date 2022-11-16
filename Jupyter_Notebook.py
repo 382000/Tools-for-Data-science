370191df9e2b3213e21329d52a07d77b315b95df
382000 <112431579+382000@users.noreply.github.com>
2022-11-16 21:56:42 +0200

    Add files via upload

---
+++ Jupyter_Notebook.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo"  />
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# <h1>My Jupyter Notebook on IBM Watson Studio<h1>
+
+# <b>Mohaned Kamal</b>
+# Dream job is Data scientist
+
+# <i>I am interested in data science because it allows me to think a lot</i>
+# <i>It allowa pushes me to the right place of thinking</i>
+
+# <h3>My code will be very very basic it only contains hello world , addaition operation<h3>
+
+# In[3]:
+
+
+print("Hello world")
+x=3
+y=4
+x+y
+
+
+# In[ ]:
+
+
+
+
